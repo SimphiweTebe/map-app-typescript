63a8082ea5129ad8d15bbaf008dc51ab2c347ef1
Simphiwe Tebe <simphiwetebe@gmail.com>
2022-02-04 16:19:50 +0200

    Created a map class with interface so that User and company can qualify to be used in Map class since they share similar properties

---
+++ src/CustomMap.ts
@@ -0,0 +1,27 @@
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    }
+}
+
+export class CustomMap{
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string){
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: { lat: 0, lng: 0}
+        });
+    }
+
+    addMarker(item: Mappable): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: item.location.lat,
+                lng: item.location.lng
+            }
+        })
+    }
+}
